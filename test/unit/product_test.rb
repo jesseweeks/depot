a765cd7b4b6d789dc9b912c22942fcd3d42c5c28
Jesse A. Weeks <business@jesseweeks.net>
2012-11-23 23:14:05 -0500

    'Validation!'

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
+	fixtures :products
  
   test "product attributes must not be empty" do
   	product = Product.new
@@ -44,4 +45,16 @@ class ProductTest < ActiveSupport::TestCase
   		assert new_product(name).valid?, "#{name} shouldn't be valid"
   	end
   end
+
+  test "product is not valid without a unique title - I18n" do
+  	product = Product.new(:title => products(:ruby).title,
+  							:description => "yyy",
+  							:price => 1,
+  							:image_url => "fred.gif")
+  	assert product.invalid?
+  	#assert_equal "has already been taken", product.errors[:title].join('; ')
+  	assert_equal [I18n.translate('activerecord.errors.messages.taken')],
+  					product.errors[:title]
+  	end
+
 end
